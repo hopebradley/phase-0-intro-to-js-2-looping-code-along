dc1c410c350c73ed28431e6e2e96276fdbe7b355
Hope Bradley <hope.e.bradley@gmail.com>
2021-02-09 14:20:27 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+
+function writeCards(names, eventName) {
+	const ans = [];
+	for (let i = 0; i<names.length; i++) {
+		ans.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+	}
+	return ans;
+}
+
+function countDown(num) {
+	for (let i = num; i>=0; i--) {
+		console.log(i);
+	}
+}
